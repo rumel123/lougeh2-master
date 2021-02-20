783c7de95c91867e19826643e9d45a4c98177123
TigerHeart123 <rumel.cortez@gmail.com>
2021-02-21 03:37:18 +0800

    first commit

---
+++ src/use-case/user/app.js
@@ -0,0 +1,19 @@
+const { patchUsers,makeUsers,enterUsers } = require('../../entities/user/app')
+const query = require('../../data-access/connection/user/app')
+
+const createUser = require('./createUser')
+const readUser = require('./readUser')
+const updateUser = require('./updateUser')
+const deleteUser = require('./deleteUser')
+
+const createUsers = createUser({makeUsers,query})
+const readUsers = readUser({enterUsers,query})
+const updateUsers = updateUser({patchUsers,query})
+const deleteUsers = deleteUser({query}) 
+
+const services = Object.freeze({
+    createUsers,readUsers,updateUsers,deleteUsers
+})
+
+module.exports = services
+module.exports = {createUsers,readUsers,updateUsers,deleteUsers}
